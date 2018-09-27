b341e2e092e25c7137b035c27280efbef0e6126a
igortsallagov <43111727+igortsallagov@users.noreply.github.com>
2018-09-27 12:36:20 +0300

    Update netology-10.py

--- netology-10.py
+++ netology-10.py
@@ -1,44 +1,49 @@
 import requests
-from pprint import pprint
 import json
 
 TOKEN = 'e580d5b70e92710bbcef6ea5e700b728d6a7b06d284f88ba9fc37f47d852d803d395d03e94084ca27a628'
-
-id_1 = 609017
-id_2 = 685162
+VERSION = '5.85'
 
 
 class User:
 
-    def __init__(self, token, vk_id):
-        self.token = token
+    def __init__(self, vk_id):
         self.vk_id = vk_id
+        self.domain = None
 
-    def get_name(self, vk_id):
-        params_users = dict(access_token=TOKEN, user_ids=vk_id, v='5.85')
-        get_name_response = requests.get('https://api.vk.com/method/users.get', params_users)
+    def get_name(self):
+        params_name = dict(access_token=TOKEN, user_ids=self.vk_id, v=VERSION)
+        get_name_response = requests.get('https://api.vk.com/method/users.get', params_name)
         self.f_name = get_name_response.json()['response'][0]['first_name']
         self.l_name = get_name_response.json()['response'][0]['last_name']
-        return print(self.f_name, self.l_name)
+        return self.f_name, self.l_name
 
-    def mutual_friends(self, vk_id_1, vk_id_2):
-        params_mutual = dict(access_token=TOKEN, source_uid=vk_id_1, target_uid=vk_id_2, v='5.85')
+    def get_domain(self):
+        params_domain = dict(access_token=TOKEN, user_ids=self.vk_id, fields='domain', v=VERSION)
+        get_name_response = requests.get('https://api.vk.com/method/users.get', params_domain)
+        self.domain = get_name_response.json()['response'][0]['domain']
+        return self.domain
+
+    def mutual_friends(self, vk_id_2):
+        self.vk_id_2 = vk_id_2
+        params_mutual = dict(access_token=TOKEN, source_uid=self.vk_id, target_uid=vk_id_2, v=VERSION)
         mutual_friends_response = requests.get('https://api.vk.com/method/friends.getMutual', params_mutual).text
         self.mutual_friends_list = json.loads(mutual_friends_response)['response']
-        return print(self.mutual_friends_list)
+        mutual_friends_as_users = [User(item) for item in self.mutual_friends_list]
+        return mutual_friends_as_users
 
+    # метод __and__ дает аналогичный результат
 
-    # for i in mutual_friends_list[7:9]:
-    #    get_name(i)
-    # for item in mutual_friends_list:
-    #    print(item, get_name(item))
+    def __and__(self, other):
+        params_and_self = dict(access_token=TOKEN, user_id=self.vk_id, v=VERSION)
+        params_and_other = dict(access_token=TOKEN, user_id=other, v=VERSION)
+        friends_self_response = requests.get('https://api.vk.com/method/friends.get', params_and_self).text
+        friends_other_response = requests.get('https://api.vk.com/method/friends.get', params_and_other).text
+        friends_self_set = set(json.loads(friends_self_response)['response']['items'])
+        friends_other_set = set(json.loads(friends_other_response)['response']['items'])
+        mutual_friends = friends_self_set & friends_other_set
+        mutual_friends_as_users = [User(item) for item in mutual_friends]
+        return mutual_friends_as_users
 
-
-mutual_friends(609017, 8118808)
-
-# friends.get
-# friends.getMutual
-# 609017 - Anastasia Glagoleva
-# 685162 - Ksenia Rozhkova
-
-a_glagoleva = 
+    def get_link(self):
+        return f'https://vk.com/id{self.vk_id}'
